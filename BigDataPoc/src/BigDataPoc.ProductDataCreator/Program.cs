a63a9b75ef01b2ab286d6f1e0870c83b5ccefdf1
bbindae <bbindae@hotmail.com>
2019-04-16 22:00:43 -0700

    Temp Build Fix

--- BigDataPoc/src/BigDataPoc.ProductDataCreator/Program.cs
+++ BigDataPoc/src/BigDataPoc.ProductDataCreator/Program.cs
@@ -14,38 +14,38 @@ namespace BigDataPoc.ProductDataCreator
             }
         }
 
-        static ProductEvent CreateProductEvent()
-        {
-            var e = new ProductEvent()
-            {
+        //static ProductEvent CreateProductEvent()
+        //{
+        //    var e = new ProductEvent()
+        //    {
 
-            }
-        }
+        //    }
+        //}
 
-        public DateTime GetRandomDate(int max)
-        {
-            var random = new Random();
+        //public DateTime GetRandomDate(int max)
+        //{
+        //    var random = new Random();
 
-        }
-        public static int RandomNumber(int min, int max)
-        {
-            var random = new Random();
-            return random.Next(min, max);
-        }
+        //}
+        //public static int RandomNumber(int min, int max)
+        //{
+        //    var random = new Random();
+        //    return random.Next(min, max);
+        //}
 
-        public static char RandomChar()
-        {
-            var n = RandomNumber(97, 122);
-            var c = Convert.ToChar(n);
-            return c;
-        }
+        //public static char RandomChar()
+        //{
+        //    var n = RandomNumber(97, 122);
+        //    var c = Convert.ToChar(n);
+        //    return c;
+        //}
 
-        public string RandomString(int size, bool lowerCase)
-        {
-            var random = new Random();
-            var ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+        //public string RandomString(int size, bool lowerCase)
+        //{
+        //    var random = new Random();
+        //    var ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
 
-        }
+        //}
     }
 
     public class ProductEvent
