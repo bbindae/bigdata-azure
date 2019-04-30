6edd31fb25b3459c7249854c2d42645814767db6
bbindae <bbindae@hotmail.com>
2019-04-29 20:52:40 -0700

    adding examples

--- BigDataPoc/docker/jupyter/start-driver.bat
+++ BigDataPoc/docker/jupyter/start-driver.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 ECHO start spark driver
-docker run --rm -it -p 8888:8888 -v "D:/DevStudio/Docker/Project/Spark:/home/jovyan" --network jupyter_spark-network bigdatapoc/spark-jupyter:latest /bin/bash
+docker run --rm -it -v "C:/DevStudio/Docker/Project/Jupyter:/home/jovyan" --network jupyter_spark-network bigdatapoc/spark-jupyter:latest /bin/bash
--- BigDataPoc/docker/jupyter/start-jupyter.bat
+++ BigDataPoc/docker/jupyter/start-jupyter.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 ECHO start jupyter notebook 
-docker run --rm -it -p 8888:8888 -v "D:/DevStudio/Docker/Project/Jupyter:/home/jovyan" --network jupyter_spark-network bigdatapoc/spark-jupyter:latest
+docker run --rm -it -p 18888:8888 -v "C:/DevStudio/Docker/Project/Jupyter:/home/jovyan/work" --network jupyter_spark-network bigdatapoc/spark-jupyter:latest
