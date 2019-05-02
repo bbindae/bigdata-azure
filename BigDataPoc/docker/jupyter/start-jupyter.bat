5d473d58e8e013ded67f37536a1e175fbd52a92b
Hyuk Kim <bbindae@hotmail.com>
2019-05-01 18:32:42 -0700

    updating start-jupyter.bat to read avro

--- BigDataPoc/docker/jupyter/start-jupyter.bat
+++ BigDataPoc/docker/jupyter/start-jupyter.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 ECHO start jupyter notebook 
-docker run --rm -it -p 18888:8888 -v "C:/DevStudio/Docker/Project/Jupyter:/home/jovyan/work" --network jupyter_spark-network bigdatapoc/spark-jupyter:latest
+docker run --rm -it -p 18888:8888 -v "D:/DevStudio/Docker/Project/Jupyter:/home/jovyan/work" -e PYSPARK_SUBMIT_ARGS="--packages org.apache.spark:spark-avro_2.12:2.4.2 pyspark-shell" --network jupyter_spark-network bigdatapoc/spark-jupyter:latest
